6ebbfcb233f2bfad4dfd03d964013096ea142750
Peter Jones <pjones@devalot.com>
2020-04-27 13:35:01 -0700

    Start using niv, build with GHC 8.6.5 and 8.8.3

--- default.nix
+++ default.nix
@@ -1,34 +1,24 @@
-{ # Pinned nixpkgs commit:
-  commit ? "516e3c1440e61abf38e5af2f60271ef385189578"
+{ sources ? import ./nix/sources.nix
+, pkgs ? import sources.nixpkgs { config = { allowBroken = true; }; }
+, lsp-test ? sources.lsp-test
+, ghcide ? sources.ghcide
+, ghc ? "ghc883"
 }:
 
 let
-  url = "https://github.com/nixos/nixpkgs/archive/${commit}.tar.gz";
-  name = "nixpkgs-${commit}";
-
-  pkgs = import (fetchTarball {inherit url name;}) {
-    config = { allowBroken = true; };
-  };
-
-  ghcideAttrs = pkgs.lib.importJSON ./ghcide.json;
-  lsptestAttrs = pkgs.lib.importJSON ./lsp-test.json;
-
   # Haskell overrides to build latest ghcide:
   overrides = self: super: with pkgs.haskell.lib; {
     hie-bios = dontCheck super.hie-bios;
     haskell-lsp = super.haskell-lsp_0_21_0_0;
     haskell-lsp-types = super.haskell-lsp-types_0_21_0_0;
-
-    lsp-test = dontCheck (super.callCabal2nix "lsp-test"
-      (fetchGit { inherit (lsptestAttrs) url rev;}) {});
+    lsp-test = dontCheck (super.callCabal2nix "lsp-test" lsp-test {});
 
     ghcide = justStaticExecutables (
-      dontCheck (super.callCabal2nix "ghcide"
-        (fetchGit {inherit (ghcideAttrs) url rev;}) {}));
+      dontCheck (super.callCabal2nix "ghcide" ghcide {}));
   };
 
   # Perform the overrides:
-  haskell = pkgs.haskellPackages.override (orig: {
+  haskell = pkgs.haskell.packages."${ghc}".override (orig: {
     overrides = pkgs.lib.composeExtensions
                  (orig.overrides or (_: _: {}))
                  overrides;
